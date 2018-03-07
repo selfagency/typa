9ee4764742d6ddbd4020e8664058b363b9bcf503
Daniel Sieradski <daniel@self.agency>
2018-03-07 14:52:03 -0500

    fleshed out

---
+++ README.md
@@ -0,0 +1,139 @@
+Typa
+====
+
+A simple JavaScript type checker. Mainly pilfered from [this blog post](https://www.webbjocke.com/javascript-check-data-types/) by [Webbjocke](https://github.com/webbjocke).
+
+**Install**
+```
+  npm install typa
+```
+
+**Import**
+```
+  import {is} from 'typa'
+```
+
+Typa Function
+-------------
+
+Checks if value matches type and then executes first function if true or second function if false.
+
+```
+  $str = 'hello'
+  $arr = 'hello'
+
+  $fn1 = (() => console.log('hello'))
+  $fn2 = (() => console.log('goodbye'))
+
+  is.typa('string', $str, $fn1, $fn2)
+    // => 'hello'
+
+  is.typa('array', $arr, $fn1, $fn2)
+    // => 'goodbye'
+```
+
+Individual Checks
+-----------------
+
+
+**Array**
+```
+  is.arr(['text', 12])
+    // => true
+```
+
+**Boolean**
+```
+  is.bool(true)
+    // => true
+
+  is.bool(false)
+    // => true
+```
+
+**Date**
+```
+  is.date(new Date())
+    // => true
+```
+
+**Empty**
+```
+  is.empty('')
+    // => true
+
+  is.empty([])
+    // => true
+
+  is.empty({})
+    // => true
+```
+
+**Error**
+```
+  is.err(new Error('This is an error.'))
+    // => true
+```
+
+**Function**
+```
+  is.fn(() => { console.log('Hi!') })
+    // => true
+```
+
+**Integer**
+```
+  is.int(12)
+    // => true
+```
+
+**JSON**
+```
+  is.json('{"key": "value"}')
+    // => true
+```
+
+**Null**
+```
+  is.nll(null)
+    // => true
+```
+
+**Null or Undefined**
+```
+  is.noru(null)
+    // => true
+
+  is.noru(undefined)
+    // => true
+```
+
+**Object**
+```
+  is.obj({ key: 'value' })
+    // => true
+```
+
+**Regex**
+```
+  is.regex(new Regex(/\W/))
+    // => true
+```
+
+**String**
+```
+  is.str('text')
+    // => true
+```
+
+**Symbol**
+```
+  is.sym(Symbol(42))
+    // => true
+```
+
+**Undefined**
+```
+  is.undef(undefined)
+    // => true
+```
--- typa.js
+++ typa.js
@@ -1,68 +1,109 @@
-// super simple type checker, with code pilfered from https://www.webbjocke.com/javascript-check-data-types/
+// array
+function arr (value) {
+  return value && typeof value === 'object' && value.constructor === Array
+}
 
-module.exports = {
-  // string
-  str (value) {
-    return typeof value === 'string' || value instanceof String
-  },
+// boolean
+function bool (value) {
+  return typeof value === 'boolean'
+}
 
-  // integer
-  int (value) {
-    return typeof value === 'number' && isFinite(value)
-  },
+// empty
+function empty (value) {
+  return value === '' || value === [] || value === {}
+}
 
-  // array
-  arr (value) {
-    return value && typeof value === 'object' && value.constructor === Array
-  },
+// date
+function date (value) {
+  return value instanceof Date
+}
 
-  // function
-  fn (value) {
-    return typeof value === 'function'
-  },
+// error
+function err (value) {
+  return value instanceof Error && typeof value.message !== 'undefined'
+}
 
-  // object
-  obj (value) {
-    return value && typeof value === 'object' && value.constructor === Object
-  },
-
-  // null
-  null (value) {
-    return value == null
-  },
-
-  // undefined
-  undef (value) {
-    return typeof value === 'undefined'
-  },
-
-  // null or undefined
-  noru (value) {
-    return value == null || typeof value === 'undefined'
-  },
-
-  // boolean
-  bool (value) {
-    return typeof value === 'boolean'
-  },
-
-  // regex
-  regex (value) {
-    return value && typeof value === 'object' && value.constructor === RegExp
-  },
-
-  // error
-  err (value) {
-    return value instanceof Error && typeof value.message !== 'undefined'
-  },
-
-  // date
-  date (value) {
-    return value instanceof Date
-  },
-
-  // symbol
-  sym (value) {
-    return typeof value === 'symbol'
+// json
+function json (value) {
+  try {
+    JSON.parse(value)
+    return true
+  } catch (e) {
+    return false
   }
 }
+
+// function
+function fn (value) {
+  return typeof value === 'function'
+}
+
+// integer
+function int (value) {
+  return typeof value === 'number' && isFinite(value)
+}
+
+// null
+function nll (value) {
+  return value == null
+}
+
+// null or undefined
+function noru (value) {
+  return value == null || typeof value === 'undefined'
+}
+
+// object
+function obj (value) {
+  return value && typeof value === 'object' && value.constructor === Object
+}
+
+// regex
+function regex (value) {
+  return value && typeof value === 'object' && value.constructor === RegExp
+}
+
+// string
+function str (value) {
+  return typeof value === 'string' || value instanceof String
+}
+
+// symbol
+function sym (value) {
+  return typeof value === 'symbol'
+}
+
+// undefined
+function undef (value) {
+  return typeof value === 'undefined'
+}
+
+// if type of $value is true, $fn1() else $fn2()
+function typa (check, value, fn1, fn2) {
+  if (fn(check) && fn(fn1) && fn(fn2)) {
+    return check(value) ? fn1 : fn2
+  } else {
+    throw new Error('Invalid parameters.')
+  }
+}
+
+const is = {
+  arr,
+  bool,
+  date,
+  empty,
+  err,
+  fn,
+  int,
+  json,
+  nll,
+  noru,
+  obj,
+  regex,
+  str,
+  sym,
+  typa,
+  undef
+}
+
+module.exports = is
