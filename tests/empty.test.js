1d83a0f74c15228b509e6b728348da00ef52b775
Daniel Sieradski <daniel@self.agency>
2018-03-07 20:38:18 -0500

    fix code style issue

--- tests/empty.test.js
+++ tests/empty.test.js
@@ -6,5 +6,5 @@ test('checks for an empty string, array, or object', () => {
   expect(is.empty({})).toBe(true)
   expect(is.empty('abc')).toBe(false)
   expect(is.empty(['a', 'b', 'c'])).toBe(false)
-  expect(is.empty({ "key": "value" })).toBe(false) // eslint-ignore-line
+  expect(is.empty({ "key": "value" })).toBe(false) // eslint-disable-line
 })
