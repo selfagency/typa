96f74df81ef1fe6b1a3c46057c131fd98bb746fb
Daniel Sieradski <daniel@self.agency>
2020-04-30 04:03:58 -0400

    update typa, test

--- tests/typa.test.js
+++ tests/typa.test.js
@@ -5,9 +5,17 @@ const b = () => false
 
 test('test typa function', () => {
   expect(is.typa('int', 123, a, b)).toBe(true)
-  expect(is.typa('int', 'abc', a, b)).toBe(false)
+  expect(is.typa('str', 123, a, b)).toBe(false)
+
   expect(is.typa('str', 'abc', a, b)).toBe(true)
   expect(is.typa('str', 123, a, b)).toBe(false)
+
+  expect(is.typa('str', 'abc', 't', 'f')).toBe('t')
+  expect(is.typa('str', 123, 't', 'f')).toBe('f')
+
+  expect(is.typa('int', 123, 't', 'f')).toBe('t')
+  expect(is.typa('int', 'abc', 't', 'f')).toBe('f')
+
   expect(() => is.typa('str')).toThrow()
   expect(() => is.typa('str', 123)).toThrow()
   expect(() => is.typa('str', 123, 'abc')).toThrow()
